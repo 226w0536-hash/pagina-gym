0244d0386de6b2464a39d9496a1781f3faf6a394
226w0536-hash <226w0536@zongolica.tecnm.mx>
2026-06-04 09:30:59 -0600

    Update equipo.php

--- recuperacion/equipo.php
+++ recuperacion/equipo.php
@@ -29,7 +29,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuestro Staff</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="equipo-body">
